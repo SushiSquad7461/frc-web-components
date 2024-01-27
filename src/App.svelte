ec56efa30752e742786bf991e710e31b631454f2
lodhasid <siddhant.lodha6@gmail.com>
2024-01-27 12:30:16 -0800

    frc web components

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { setNt4Context, getNt4Context } from "@frc-web-components/svelte";
+  import ThemeChooser from "./components/ThemeChooser.svelte";
+  import Dashboard from "./components/Dashboard.svelte";
+  import { getEntry } from "@frc-web-components/svelte";
+
+  // Set robot IP address here
+  setNt4Context('7461');
+
+</script>
+
+<div class="App">
+  <ThemeChooser />
+  <Dashboard />
+</div>
